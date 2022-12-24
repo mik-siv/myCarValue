103db40c4408bcc2a8e7a24ff69f836b413576b5
Sivinskikh Mikhail <mikhail.sivinskikh@gmail.com>
2022-12-24 18:53:24 +0200

    continued implementing users service repository logic

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,11 +1,33 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  AfterInsert,
+  AfterRemove,
+  AfterUpdate,
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
-  id: string;
+  id: number;
   @Column()
   email: string;
   @Column()
   password: string;
+
+  @AfterInsert()
+  logInsert() {
+    console.log('Inserted a user with id', this.id);
+  }
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log('Updated a user with id', this.id);
+  }
+
+  @AfterRemove()
+  logRemove() {
+    console.log('Removed a user with id', this.id);
+  }
 }
