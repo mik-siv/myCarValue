3ee973064955819399ec14f6af5e62204c3777e3
Sivinskikh Mikhail <mikhail.sivinskikh@gmail.com>
2023-01-13 22:14:09 +0200

    moved cookie key to env variable

--- src/app.module.ts
+++ src/app.module.ts
@@ -28,12 +28,6 @@ const cookieSession = require('cookie-session');
         };
       },
     }),
-    // TypeOrmModule.forRoot({
-    //   type: 'sqlite',
-    //   database: 'db.sqlite',
-    //   entities: [User, Report],
-    //   synchronize: true,
-    // }),
     UsersModule,
     ReportsModule,
   ],
@@ -49,11 +43,12 @@ const cookieSession = require('cookie-session');
   ],
 })
 export class AppModule {
+  constructor(private configService: ConfigService) {}
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(
         cookieSession({
-          keys: ['kygdsvjk'],
+          keys: [this.configService.get('COOKIE_KEY')],
         }),
       )
       .forRoutes('*');
