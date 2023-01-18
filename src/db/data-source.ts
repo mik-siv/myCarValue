c80a87666ec85c8da342ed211c715ed0cb13f012
Sivinskikh Mikhail <mikhail.sivinskikh@gmail.com>
2023-01-18 22:41:09 +0200

    updated project for the prod deployment

--- src/db/data-source.ts
+++ src/db/data-source.ts
@@ -1,26 +1,44 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
 
-const getDbName = (env: string) => {
+const getDbConfig = (env: string): DataSourceOptions => {
   switch (env) {
     case 'development':
-      return 'db.sqlite';
+      return {
+        type: 'sqlite',
+        synchronize: false,
+        entities: [__dirname + '/../**/*.entity.{js,ts}'],
+        database: 'db.sqlite',
+        migrations: ['dist/db/migrations/*.js'],
+      };
     case 'test':
-      return 'test.sqlite';
+      return {
+        type: 'sqlite',
+        synchronize: false,
+        entities: [__dirname + '/../**/*.entity.{js,ts}'],
+        database: 'test.sqlite',
+        migrations: ['dist/db/migrations/*.js'],
+        migrationsRun: true,
+      };
     case 'production':
-      break;
+      return {
+        type: 'postgres',
+        synchronize: false,
+        url: process.env.DATABASE_URL,
+        entities: [__dirname + '/../**/*.entity.{js,ts}'],
+        migrations: ['dist/db/migrations/*.js'],
+        migrationsRun: true,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      };
     default:
       throw new Error('unknown environment');
   }
 };
 
-export const dataSourceOptions: DataSourceOptions = {
-  type: 'sqlite',
-  synchronize: false,
-  entities: [__dirname + '/../**/*.entity.{js,ts}'],
-  database: getDbName(process.env.NODE_ENV),
-  migrations: ['dist/db/migrations/*.js'],
-  migrationsRun: process.env.NODE_ENV === 'test' ? true : false,
-};
+export const dataSourceOptions: DataSourceOptions = getDbConfig(
+  process.env.NODE_ENV,
+);
 
 const dataSource = new DataSource(dataSourceOptions);
 
